e37a0bbf1ef07be4de0412df48a3015d3d878163
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2024-07-26 22:01:18 +0900

    15

--- app/Policies/ShopPolicy.php
+++ app/Policies/ShopPolicy.php
@@ -5,62 +5,49 @@ namespace App\Policies;
 use App\Models\Shop;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Auth\Access\HandlesAuthorization;
 
 class ShopPolicy
 {
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+
+    use HandlesAuthorization;
+
+    public function before($user, $ability)
+    {
+        if ($user->hasRole('admin')) {
+            return true;
+        }
+    }
+
+    public function browse(User $user)
+    {
+        return $user->hasRole('seller');
+    }
+
+    public function read(User $user, Shop $shop)
+    {
+        return $user->id == $shop->user_id;
+    }
+
+    public function edit(User $user, Shop $shop)
+    {
+        return $user->id == $shop->user_id;
+    }
+
+    public function add(User $user)
     {
         //
     }
 
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Shop $shop): bool
+    public function delete(User $user, Shop $shop)
     {
-        //
+        return $user->id == $shop->user_id;
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
+    
 
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Shop $shop): bool
-    {
-        //
-    }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Shop $shop): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Shop $shop): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Shop $shop): bool
-    {
-        //
-    }
 }
