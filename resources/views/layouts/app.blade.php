11677eaca51b03e247ceadab964774349dab6c93
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2024-09-19 21:17:46 +0900

    49

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body ontouchstart=””>
+<body ontouchstart="">
     <div id="app">
         
         <!-- header part -->
