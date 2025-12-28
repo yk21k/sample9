615f4dfe8960dae3b61cce2dccbcc48567e171da
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2025-12-28 18:19:50 +0900

    136

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-console.log(laravel); // Check what is being imported
-
 export default defineConfig({
-  plugins: [
-    laravel.default(), // Access the 'default' export and call the function
-  ],
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+        }),
+    ],
 });
-
-// Export the Vite config using ESM syntax
-// export default defineConfig({
-//   plugins: [laravel()],
-// });
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: [
-//                 'resources/sass/app.scss',
-//                 'resources/sass/product-detail.scss',
-//                 'resources/js/app.js',
-//             ],
-//             refresh: true,
-//         }),
-//     ],
-
-// });
