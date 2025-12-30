5bed2d800543fc42a0c972faf0179d2d944e448e
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2025-12-30 22:29:40 +0900

    142

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\Faq;
+use App\Models\FAQ;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
