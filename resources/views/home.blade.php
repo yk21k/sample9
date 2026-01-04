5731dbcb7fad406412e93e5bc1f1a31a0c2503e3
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2026-01-04 20:43:17 +0900

    212

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -137,29 +137,6 @@
 
     <a href="{{ route('entrance') }}"><h4>案内板へ戻る</h4></a>
 
-    {{-- ===== Holiday Information ===== --}}
-    <section class="holiday-info">
-        <div class="text-animation1">
-            @foreach($holidays as $holiday)
-                <p>Holiday Store: {{ $holiday['shop_name'] }}</p>
-            @endforeach
-        </div>
-
-        @foreach($extra_holidays as $ex_holiday)
-            @if($ex_holiday['date_flag'] === 2)
-                <div class="text-animation2">
-                    <p>Stores Temporarily Closed :: {{ $ex_holiday['shop_name'] }}</p>
-                </div>
-            @endif
-
-            @if($ex_holiday['date_flag'] === 1)
-                <div class="text-animation3">
-                    <p>📣📣📣 Temporary Store :: {{ $ex_holiday['shop_name'] }} 📣📣📣</p>
-                </div>
-            @endif
-        @endforeach
-    </section>
-
     {{-- ===== Product Ranking ===== --}}
     @php
         $products = collect();
@@ -215,6 +192,29 @@
                         </div>
                     @endforeach
                 </div>
+            </section>
+
+            {{-- ===== Holiday Information ===== --}}
+            <section class="holiday-info">
+                <div class="text-animation1">
+                    @foreach($holidays as $holiday)
+                        <p>Holiday Store: {{ $holiday['shop_name'] }}</p>
+                    @endforeach
+                </div>
+
+                @foreach($extra_holidays as $ex_holiday)
+                    @if($ex_holiday['date_flag'] === 2)
+                        <div class="text-animation2">
+                            <p>Stores Temporarily Closed :: {{ $ex_holiday['shop_name'] }}</p>
+                        </div>
+                    @endif
+
+                    @if($ex_holiday['date_flag'] === 1)
+                        <div class="text-animation3">
+                            <p>📣📣📣 Temporary Store :: {{ $ex_holiday['shop_name'] }} 📣📣📣</p>
+                        </div>
+                    @endif
+                @endforeach
             </section>
 
         {{-- ===== Other Products ===== --}}
