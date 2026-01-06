82ce0249c8fa01258a5f8c3aa6bc4877f58a644c
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2026-01-06 22:46:18 +0900

    230

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -165,7 +165,6 @@
           })
         })()
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
