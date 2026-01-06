557e4358f9d83aa14b02aa3672082d19052e8271
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2026-01-06 22:54:33 +0900

    231

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,23 +30,20 @@
     <link rel="stylesheet" href="{{ asset('front/css/custom95.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-/* bd-mode-toggle 自体の当たり判定を限定 */
-.bd-mode-toggle {
-    width: auto;
-    height: auto;
-    pointer-events: auto;
-    z-index: 1050;
-}
+        /* bd-mode-toggle 自体の当たり判定を限定 */
+        .bd-mode-toggle {
+            width: auto;
+            height: auto;
+            pointer-events: auto;
+            z-index: 1050;
+        }
 
-/* SVGは絶対にイベントを取らせない */
-.bd-mode-toggle svg,
-.bd-mode-toggle use {
-    pointer-events: none !important;
-}
-.bd-mode-toggle {
-    outline: 3px solid red !important;
-    background: rgba(255,0,0,0.05);
-}
+        /* SVGは絶対にイベントを取らせない */
+        .bd-mode-toggle svg,
+        .bd-mode-toggle use {
+            pointer-events: none !important;
+        }
+
     </style>
 
     @stack('css')
@@ -173,7 +170,7 @@
 <body ontouchstart="">
     
     <!-- Icon settings -->
-<!--     <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+    <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
       <symbol id="circle-half" viewBox="0 0 16 16">
         <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
       </symbol>
@@ -184,7 +181,7 @@
       <symbol id="sun-fill" viewBox="0 0 16 16">
         <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
       </symbol>
-    </svg> -->
+    </svg>
     
     <!-- drop down button -->
     <div class="dropdown position-fixed mb-3 me-3 bd-mode-toggle">
