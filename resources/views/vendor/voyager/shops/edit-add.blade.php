1aff0542e7c93478001a166fe667e59b9c617a2e
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2026-01-07 13:24:07 +0900

    235

--- resources/views/vendor/voyager/shops/edit-add.blade.php
+++ resources/views/vendor/voyager/shops/edit-add.blade.php
@@ -27,6 +27,19 @@
                 <a href="{{url('/seller/orders')}}" class="">Order Management</a>
             </div>
 
+            <td>
+                @if($dataTypeContent->{$row->field} && auth()->user()->hasRole('admin'))
+                    <a class="btn btn-sm btn-primary" 
+                       href="{{ route('admin.shop-license.show', [$dataTypeContent->id, $dataTypeContent->{$row->field}]) }}" 
+                       target="_blank">
+                        ファイル確認
+                    </a>
+                @else
+                    <span class="text-muted">非表示</span>
+                @endif
+            </td>
+
+
             <div class="col-md-12">
 
                 <div class="panel panel-bordered">
--- resources/views/vendor/voyager/shops/license_link.blade.php
+++
@@ -1,9 +0,0 @@
-@if($dataTypeContent->{$row->field} && auth()->user()->hasRole('admin'))
-    <a class="btn btn-sm btn-primary" 
-       href="{{ route('admin.shop-license.show', [$dataTypeContent->id, $dataTypeContent->{$row->field}]) }}" 
-       target="_blank">
-        ファイル確認
-    </a>
-@else
-    <span class="text-muted">非表示</span>
-@endif
